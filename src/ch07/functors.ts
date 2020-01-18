fd8e8ca37a52fe58ce9ce2b3b763d7e09a135f9f
kei <tenguuenglish@gmail.com>
2020-01-19 00:09:54 +0900

    :sparkles: Complete functors

---
+++ src/ch07/functors.ts
@@ -0,0 +1,16 @@
+class Container<T> {
+  private _value: T;
+
+  public constructor(val: T) {
+    this._value = val;
+  }
+
+  public map<TMap>(fn: (val: T) => TMap) {
+    return new Container<TMap>(fn(this._value));
+  }
+}
+
+const double = (x: number) => x + x;
+const container = new Container(3);
+const container2 = container.map(double);
+console.log(container2);
